5b1c5ce8cef24d4ff80322ecf3c9703a92e910f5
FunkierCoder FUNKIER <contactosjuandm@gmail.com>
2023-11-23 18:56:25 -0700

    Agregar archivos de proyecto.

---
+++ ListaPerros/Lista.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaPerros
+{
+    public class Lista
+    {
+        public Nodo Primero;
+        public Nodo Ultimo;
+
+        public Lista() 
+        {
+            Primero = null;
+            Ultimo = null;
+        }
+        public void insertar(Perro perro)
+        {
+            Nodo Nuevo = new Nodo(perro);
+
+            if(Primero == null)
+            {
+                Primero = Nuevo;
+                Primero.Siguiente = null;
+                Ultimo = Nuevo;
+            }
+            else
+            {
+                Ultimo.Siguiente = Nuevo;
+                Nuevo.Siguiente = null;
+                Ultimo = Nuevo;
+            }
+            Console.WriteLine("Nodo ingresado");
+        }
+        public void MostrarPerro()
+        {
+            Nodo Actual = Primero;
+            if (Primero != null)
+            {
+                while (Actual != null)
+                {
+                    Console.WriteLine($"El nombre del perro es {Actual.DatosPerro.Nombre} y  su edad es {Actual.DatosPerro.edad}");
+                    Actual = Actual.Siguiente;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Lista vacia");
+            }
+        }
+        public void buscarPerro(string nombrePerro)
+        {
+            Nodo Actual = Primero;
+            bool encontrado = false;
+            if (Primero != null)
+            {
+                while(Actual != null)
+                {
+                    if (Actual.DatosPerro.Nombre == nombrePerro)
+                    {
+                        Console.WriteLine("El perro con el nombre {0} FUE ENCONTRADO" , Actual.DatosPerro.Nombre);
+                        encontrado  = true;
+                    }
+                    Actual = Actual.Siguiente;
+                }
+                if(!encontrado)
+                {
+                    Console.WriteLine("Perro no encontrado en esta lista");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Lista vacia ");
+            }
+
+
+        }
+        public void ModificarPerro(int buscarEdad)
+        {
+            Nodo Actual = Primero;
+            bool encontrado = false;    
+            if (Primero != null)
+            {
+                while(Actual !=null)
+                {
+                    if (Actual.DatosPerro.edad == buscarEdad)
+                    {
+                        Console.WriteLine("Perro encontrado con edad {0}",buscarEdad);
+                        Console.WriteLine("Ingresa el nuevo nombre del perro");
+                        string nombrePerro = Console.ReadLine();
+                        Console.WriteLine("Ingresa nueva edada de perro");
+                        int nuevaEdada = int.Parse(Console.ReadLine());
+
+                        Actual.DatosPerro.edad = nuevaEdada;
+                        Actual.DatosPerro.Nombre = nombrePerro;
+                        Console.WriteLine("Perro modificado correctamente");
+                        encontrado = true;
+                    }
+                    Actual = Actual.Siguiente;
+                }
+                if (!encontrado)
+                {
+                    Console.WriteLine("Perro no esta en este nodo");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Lista vacia");
+            }
+            
+        }
+        public void EliminarPerro(string nombre, int edad)
+        {
+            Nodo Actual = Primero;
+            Nodo Anterior = null;
+            bool encontrado = false;
+            if (Primero != null)
+            {
+                while(Actual != null && encontrado != true)
+                {
+                   if(Actual.DatosPerro.Nombre == nombre && Actual.DatosPerro.edad == edad)
+                    {
+                        if(Actual == Primero )
+                        {
+                            Primero = Primero.Siguiente;
+                        }
+                        else if( Actual == Ultimo)
+                        {
+                            Anterior.Siguiente = null;
+                            Ultimo = Anterior;
+                        }
+                        else
+                        {
+                            Anterior.Siguiente = Actual.Siguiente;
+                        }
+                        Console.WriteLine("Perro eliminado correctamente");
+                        encontrado = true;
+                    }
+                    Anterior = Actual;
+                    Actual = Actual.Siguiente;
+                }
+                if(!encontrado)
+                {
+                    Console.WriteLine("PERRO NO ENCONTRADO");
+                }
+            }
+            else
+            {
+                Console.WriteLine("LISTA VACIA");
+            }
+
+        }
+    }
+}
---
+++ ListaPerros/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaPerros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lista listaPerros = new Lista();
+
+            int opcion = 0;
+            do
+            {
+                Console.WriteLine("1. Insertar perro");
+                Console.WriteLine("2. Mostrar lista de perros");
+                Console.WriteLine("3. Editar perro buscandolo por Edada");
+                Console.WriteLine("4. Encontrar perro");
+                Console.WriteLine("5. Eliminar perro");
+                Console.WriteLine("Ingrese la opción:");
+                opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("Ingrese nombre del perro:");
+                        string nombre = Console.ReadLine();
+                        Console.WriteLine("Ingrese edad del perro:");
+                        int edad = Convert.ToInt32(Console.ReadLine());
+                        Perro nuevoPerro = new Perro { Nombre = nombre, edad = edad };
+                        listaPerros.insertar(nuevoPerro);
+                        break;
+                        case 2:
+                        listaPerros.MostrarPerro();
+                        break;
+                        case 3:
+                        Console.WriteLine("Ingresa la edad para buscar");
+                        int edadPerro = Convert.ToInt32(Console.ReadLine());
+                        listaPerros.ModificarPerro(edadPerro);
+                        break;
+                    case 4:
+                        Console.WriteLine("Ingresa el nombre del perro");
+                        string nombreDelCan = Console.ReadLine();
+                        listaPerros.buscarPerro(nombreDelCan);
+                        break;
+                        case 5:
+                        Console.WriteLine("Ingresa Nombre y edad del perro para eliminarlo");
+                        string NombreP = Console.ReadLine();
+                        int edadP = Convert.ToInt32(Console.ReadLine());
+                        listaPerros.EliminarPerro(NombreP, edadP);
+                        break;
+
+                }
+            }while (opcion != 6);
+    Console.ReadLine();
+        }
+
+    }
+}
